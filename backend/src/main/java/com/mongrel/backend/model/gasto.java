c80c966953d819667bdc5e89af0d7aa4f76e67af
MateusTrindade133 <81597306+MateusTrindade133@users.noreply.github.com>
2026-04-15 19:09:37 +0000

    Criação da entidade 'Gasto' que será responsavel na respectiva tabela do db pelo armazenamento dos dados de gasto cadastrado

--- backend/src/main/java/com/mongrel/backend/model/gasto.java
+++ backend/src/main/java/com/mongrel/backend/model/gasto.java
@@ -1,1 +1,40 @@
 package com.mongrel.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Gasto {
+    @Id
+    @GenerateValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private Double valor;
+
+    // Construtor vazio obrigatório pro JPA //
+
+    public Gasto () {}
+
+    // getters e setters //
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}

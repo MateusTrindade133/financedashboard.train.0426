cc7a5e8fd3e93a4c400f3af057ed6f3cabdcda36
MateusTrindade133 <81597306+MateusTrindade133@users.noreply.github.com>
2026-04-28 19:24:25 +0000

    Implementaçãodo service com a regra de negocio que não permite que um valor <= 0 seja cadastrado e com a função de buscar todos os gastos e retornar uma lista.

---
+++ backend/src/main/java/com/mongrel/backend/service/GastoService.java
@@ -0,0 +1,34 @@
+package com.mongrel.backend.service;
+
+import org.springframework.stereotype.Service;
+import com.mongrel.backend.model.Gasto;
+import com.mongrel.backend.repository.GastoRepository;
+
+@Service
+public class GastoService {
+    private final GastoRepository gastoRepository;
+    //declara que o service depende do repository//
+    //private: só essa classe acessa. final: não muda apos inicializado//
+
+    public GastoService(GastoRepository gastoRepository) {
+        this.gastoRepository = gastoRepository;
+
+    //injeção de dependencia (injetar a dependencia gastoRepository dentro do service sem precisar criar ela)//
+    }
+
+    public Gasto criar(Gasto gasto) {
+        //metodo que repesenta uma ação do sistema//
+        //recebe o objeto Gasto e retorna Gasto//
+        if (gasto.getValor() <= 0) {
+            throw new
+            RuntimeException("Valor inválido");
+            //regra de negocio, se os dados forem invalidos o fluxo é interrompido e o erro lançado//
+        }
+        return gastoRepository.save(gasto);
+        //se passou na validação os dados são salvos no banco//
+    }
+    public List<Gasto> listar() {
+        return gastoRepository.findAll();
+        //busca todos os gastos, chama o repository e retorna uma lista//
+    }
+}

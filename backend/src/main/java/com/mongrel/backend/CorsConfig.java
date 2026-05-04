fc7c61ae9e333edc7a69890bea1adad9a7d9b0e4
MateusTrindade133 <81597306+MateusTrindade133@users.noreply.github.com>
2026-05-04 20:01:03 +0000

    Implementação dos imports das classes utilizadas para configuração do cors e do service, as quais sumiram de serem automaticamente utilizadas dada as mudanças entre os arquivos (especificamente troca de responsabilidade de comunicação com o repository que antes era feita pelo controller e agora é feita pelo service).

--- backend/src/main/java/com/mongrel/backend/CorsConfig.java
+++ backend/src/main/java/com/mongrel/backend/CorsConfig.java
@@ -1,5 +1,10 @@
 package com.mongrel.backend;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+
 @Configuration
 public class CorsConfig {
     @Bean
